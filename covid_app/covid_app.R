d7f29b7746a4cd1d17fb7fde24bf610d9839fc1c
kristinbryan <kbryan@macalester.edu>
2020-12-01 19:19:10 -0600

    exercises completed, could be cleaned up a little

--- covid_app/covid_app.R
+++ covid_app/covid_app.R
@@ -1,49 +1,34 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(tidyverse)
+library(rsconnect)
 
-# Define UI for application that draws a histogram
+covid19 <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    h1("COVID-19 Cases Over Time"),
+    selectInput("states", 
+                "Select the states you would like to compare:", 
+                multiple = TRUE,
+                choices = covid19$state),
+    submitButton(text = "Compare cases!"),
+    plotOutput(outputId = "timeplot")
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$timeplot <- renderPlot({
+        covid19 %>% 
+            filter(state %in% input$states, 
+                   cases >= 20) %>% 
+            mutate(days_since_20 = date - min(date)) %>% 
+            ggplot() +
+            geom_line(aes(x = days_since_20, y = cases, color = state)) + 
+            labs(x = "Days Since 20+ Cases", 
+                 y = "Cases (on the logarithmic scale)", 
+                 color = "State") + 
+            scale_y_log10(label = scales::label_comma()) +
+            theme_minimal()
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
